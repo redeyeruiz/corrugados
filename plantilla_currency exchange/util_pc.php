80ff24c96d1fb391db060b774f21504aba989c8f
AdrianTorresG <atg292001@hotmail.com>
2021-04-02 23:27:26 -0600

    agentes-php

---
+++ plantilla_currency exchange/util_pc.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "PapelesCorrugados";
+
+$conection = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
