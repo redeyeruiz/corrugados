5351e2b542905daea3f077150146fa1d0cebfcf2
AdrianTorresG <atg292001@hotmail.com>
2021-04-10 12:12:46 -0500

    cantEntre-act

--- plantilla_currency exchange/cantEntre.php
+++ plantilla_currency exchange/cantEntre.php
@@ -242,6 +242,7 @@ include("funciones/cantEntrefuncP.php");
                                     </div>
                                 </div>
                             </div>
+                            <br/>
                             <div class="center">
                                 &nbsp;
                                 &nbsp;
@@ -262,6 +263,18 @@ include("funciones/cantEntrefuncP.php");
                             <div align="center" style="color:#475747; font-size:20px;" class="success"><?= $success; ?></div>
                         </td>
                     </tr>
+                    <?php
+                        if ($btnsn != ""){
+                            echo '<tr>
+                                    <td colspan="2">
+                                        <div class="center">
+                                            <button name="confirmoc" type="submit" style="width:200px" class="btn btn-outline-success">Confirmar</button>
+                                            <button name="canceloc" type="submit" style="width:200px" class="btn btn-outline-danger">Cancelar</button>
+                                        </div>
+                                    </td>
+                                </tr>';
+                        }
+                    ?>
                 </form>
             </table>
             <?php
