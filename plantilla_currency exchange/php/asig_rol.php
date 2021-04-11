3eb832d3006db685253a0459722b36b4be5b048f
Keyuan01 <58576843+Keyuan01@users.noreply.github.com>
2021-04-10 19:34:10 -0500

    Actualización

--- plantilla_currency exchange/php/asig_rol.php
+++ plantilla_currency exchange/php/asig_rol.php
@@ -1,142 +1,112 @@
 <?php
 
-session_start();
-if(!isset($_SESSION['conectado'])){
-    $_SESSION['mens_error'] = "Por favor inicie sesión.";
-    header("Location: http://localhost/corrugados/plantilla_currency%20exchange/login.php");
-    die();
+include_once "utilerias.php";
+$id_user_error = $rol_error = ""; 
+$id_user = $rol = $success = $option = "";
+
+if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_altas"])){
+   	if (empty($_POST["id_user"])){
+        $id_user_error = "Se requiere el ID del Usuario.";
+    }
+    else{
+        $id_user = test_input($_POST["id_user"]);
+    }
+
+    if (empty($_POST["rol"])){
+        $rol_error = "Se requiere el Rol.";
+    }
+    else{
+        $rol = test_input($_POST["rol"]);
+    }
+    
+    if ($id_user_error == "" and $rol_error == ""){
+        $query="UPDATE Usuario SET rol='$rol', estatus='1' WHERE idUsuario='$id_user'";
+        $sql=mysqli_query($conection,$query);
+        if (!$sql){
+            $success = "Error en el alta de rol al Usuario.";
+        }
+        else{
+            $success = "Alta realizada con éxito.";
+        }
+        $id_user = $rol = "";
+    }
 }
 
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Asignación de usuario a roles</title>
-</head>
-<body><center>
-	<?php  
-		include_once("utilerias.php");
+if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_bajas"])){
+    if (empty($_POST["id_user"])){
+        $id_user_error = "Se requiere el ID del Usuario.";
+    }
+    else{
+        $id_user = test_input($_POST["id_user"]);
+    }
+    
+    if ($id_user_error == ""){
+        $query="UPDATE Usuario SET estatus='0' WHERE idUsuario='$id_user'";
+        $sql=mysqli_query($conection,$query);
+        if (!$sql){
+            $success = "Error en la baja del Rol.";
+        }
+        else{
+            $success = "Baja realizada con éxito.";
+        }
+        $id_comp = $rol = $rol_af = "";
+    }
+}
 
-		$op = $_GET['op'];
-		if ($op == "altas") alta_asig_rol();
-		if ($op == "bajas") baja_asig_rol();
-		if ($op == "consulta") consulta_asig_rol();
-		if ($op == "actualiza") actualiza_asig_rol();
-		if ($op == "reporte") reporte_asig_rol();
+if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_actualizar"])){
+    if (empty($_POST["id_user"])){
+        $id_user_error = "Se requiere el ID del Usuario.";
+    }
+    else{
+        $id_user = test_input($_POST["id_user"]);
+    }
+    if (empty($_POST["rol"])){
+        $rol_error = "Se requiere el Rol a asignar.";
+    }
+    else{
+        $rol = test_input($_POST["rol"]);
+    }
 
+    if ($id_user_error == "" and $rol_error == ""){
+        $query="UPDATE Usuario SET rol='$rol' WHERE idUsuario='$id_user'";
+        $sql=mysqli_query($conection,$query);
+        if (!$sql){
+            $success = "Error en la actualización de datos del Rol.";
+        }
+        else{
+            $success = "Actualización realizada con éxito.";
+        }
+        $id_comp = $rol = "";
+    }
+}
 
-		function alta_asig_rol(){
-			$id_user = $_GET['id_user'];
-			$rol = $_GET['rol'];
+if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_consultas"])){
+    if (empty($_POST["id_user"])){
+        $id_user_error = "Se requiere el ID del Usuario.";
+    }
+    else{
+        $id_user = test_input($_POST["id_user"]);
+    }
 
-			$conexion=conecta_servidor();
-			$query="UPDATE usuario SET rol='$rol' WHERE idUsuario='$id_user'";
-			$sql=mysqli_query($conexion,$query);
-			if (!$sql){
-				msg("Error, el ID Usuario se duplica en la base de datos","rojo");
-			}
-			else{
-				msg("El registro se ha grabado correctamente","verde");
-			}
-		}
+    if ($id_user_error == ""){
+        $option = "Consultas por ID del Usuario";
+        $query="SELECT * FROM Usuario WHERE idUsuario='$id_user' and estatus='1'";
+        $result = mysqli_query($conection, $query);
+        $id_user = $rol = "";
+    }
+}
 
-		function baja_asig_rol(){
-			$id_user = $_GET['id_user'];
+if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_reporte"])){
 
-			$conexion=conecta_servidor();
-			$query="UPDATE usuario SET rol='' WHERE idUsuario='$id_user'";
-			$sql=mysqli_query($conexion,$query);
-			if (mysqli_affected_rows($conexion)==0){
-				msg("Error, ID Usuario inexistente en base de datos","rojo");
-			}
-			else{
-				msg("El registro se ha eliminado correctamente","verde");
-			}
-		}
+    $option = "Reporte";
+    $query="SELECT * FROM Usuario";
+    $result = mysqli_query($conection, $query);
+    $id_user = $rol = "";
+}
 
-		function formulario($id_user, $rol){
-			echo "
-				<table border='0' width='50%' align='center'>
-					<tr>
-                        <td>
-                            <p align='center'><b>ID Usuario</b></p>
-                        </td>
-                        <td align='center'>
-                            <input style='border:3px solid #ff880e' name='id_user' type='text' size='50' maxlength='50' class='campo' value='$id_user' disabled>
-                        </td>
-                    </tr>
-					<tr>
-                        <td>
-                            <p align='center'><b>Rol</b></p>
-                        </td>
-                        <td align='center'>
-                            <input style='border:3px solid #ff880e' name='rol' type='text' size='50' maxlength='50' class='campo' value='$rol' disabled>
-                        </td>
-                    </tr>
-  		        </table>
-			";
-		}
-
-		function consulta_asig_rol(){
-			$id_user = $_GET['id_user'];
-			$conexion=conecta_servidor();
-			$query="SELECT * FROM usuario WHERE idUsuario ='$id_user'";
-			$sql=mysqli_query($conexion,$query);
-			$reg=mysqli_fetch_object($sql);
-			if ($reg==mysqli_fetch_array($sql)){
-				msg("Error, ID Usuario inexistente en base de datos","rojo");
-			}
-			else{
-				msg("Consulta realizada","verde");
-				formulario($id_user,$reg->rol);
-			}
-		}
-
-		function actualiza_asig_rol(){
-			$id_user = $_GET['id_user'];
-			$rol = $_GET['rol'];
-			$conexion=conecta_servidor();
-			$query="UPDATE usuario SET rol='$rol' WHERE idUsuario='$id_user'";
-			$sql=mysqli_query($conexion,$query);
-			if (mysqli_affected_rows($conexion)==0){
-				msg("Error, ID Usuario inexistente en la base de datos","rojo");
-			}
-			else{
-				msg("El cambio ha sido realizado","verde");
-			}
-		}
-
-		function reporte_asig_rol() {
-			$conexion=conecta_servidor();
-			$query="SELECT * FROM usuario ORDER BY idUsuario";
-			$sql=mysqli_query($conexion,$query);
-			echo "
-				<table border='3' width='80%'>
-				<tr align='center' bgcolor='#A1C1F3'>
-					<td colspan='2'>
-						<p class='texto20'>Listado de Usuario ordenado por ID</p>
-					</td>
-				</tr>
-			";
-			$cont=0;
-			while ($reg=mysqli_fetch_object($sql)){
-				echo "<tr>";
-					echo "
-						<td align='center'><font color='blue'><b>$reg->idUsuario</b></font></td>
-						<td align='center'><font color='blue'><b>$reg->rol</b></font></td>
-					";
-				echo "</tr>";
-				$cont++;
-			}
-			echo "
-				<tr align='center' bgcolor='#A1C1F3'> 
-					<td colspan='2'>
-						<p class='texto20'>Total de usuarios = $cont</p>
-					</td>
-				</tr>
-			";
-			echo "</table>";
-		}
-	?>
-</center></body>
-</html>
+function test_input($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
