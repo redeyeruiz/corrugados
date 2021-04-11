6db42524a44152abc4e65aa4c8e8f683ff9337c7
Keyuan01 <58576843+Keyuan01@users.noreply.github.com>
2021-04-10 19:33:24 -0500

    Actualización

--- plantilla_currency exchange/php/utilerias.php
+++ plantilla_currency exchange/php/utilerias.php
@@ -69,23 +69,10 @@
         } 
     }
 
-	function conecta_servidor(){
-		$conexion=mysqli_connect("localhost","root","","papelescorrugados");
-		return $conexion;
-	}
+	$dbServername = "localhost";
+	$dbUsername = "root";
+	$dbPassword = "";
+	$dbName = "PapelesCorrugados";
 
-	function msg($mensaje, $color){
-		echo "<table border='3' width='60%'> ";
-		if ($color=="rojo") echo "<tr bgcolor='red' align='center'>";
-		if ($color=="amarillo") echo "<tr bgcolor='yellow' align='center'>";
-		if ($color=="verde") echo "<tr bgcolor='green' align='center'>";
-		echo "
-				<td><font color='white' size='6'><b>$mensaje</b></font></td>
-					</tr>
-					<tr align='center'>
-						<td><font color='blue' size='6'><b>Para continuar selecciona los botones del menú</b></font></td>
-					</tr>
-			</table>
-		";
-	}
+	$conection = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 ?>
