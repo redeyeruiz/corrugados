5ddbbea2880579b919708ce0dca609c7353d8d6c
Keyuan01 <58576843+Keyuan01@users.noreply.github.com>
2021-04-24 18:01:46 -0500

    Update registro_bajas.php

--- plantilla_currency exchange/php/registro_bajas.php
+++ plantilla_currency exchange/php/registro_bajas.php
@@ -1,10 +1,10 @@
 <?php
 
 include_once "utilerias.php";
-$id_user_error = $fecha_error = ""; 
-$id_user = $fecha = $success = $option = "";
+$id_user_error = $fecha_error = $desc_error = ""; 
+$id_user = $fecha = $desc = $success = $option = "";
 
-if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_consultar"])){
+if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_registrar"])){
    	if (empty($_POST["id_user"])){
         $id_user_error = "Se requiere el ID del Usuario.";
     }
@@ -18,14 +18,45 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_consultar"])){
     else{
         $fecha = test_input($_POST["fecha"]);
     }
+
+    if (empty($_POST["desc"])){
+        $desc_error = "Se requiere la descripción.";
+    }
+    else{
+        $desc = test_input($_POST["desc"]);
+    }
     
     if ($id_user_error == ""){
         $option = "Consultas por ID del Usuario";
-        $query="SELECT * FROM Registro_bajas WHERE idUsuario='$id_user' and fecha='$fecha'";
+        $query="INSERT INTO Registro_bajas VALUES ('$id_user', '$fecha', '$desc')";
         $result = mysqli_query($conection, $query);
         $id_user = $fecha = "";
     }
 }
+
+if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_consultar"])){
+    if (empty($_POST["id_user"])){
+     $id_user_error = "Se requiere el ID del Usuario.";
+ }
+ else{
+     $id_user = test_input($_POST["id_user"]);
+ }
+
+ if (empty($_POST["fecha"])){
+     $fecha_error = "Se requiere la fecha.";
+ }
+ else{
+     $fecha = test_input($_POST["fecha"]);
+ }
+ 
+ if ($id_user_error == ""){
+     $option = "Consultas por ID del Usuario";
+     $query="SELECT * FROM Registro_bajas WHERE idUsuario='$id_user' and fecha='$fecha'";
+     $result = mysqli_query($conection, $query);
+     $id_user = $fecha = "";
+ }
+}
+
 
 function test_input($data){
     $data = trim($data);
