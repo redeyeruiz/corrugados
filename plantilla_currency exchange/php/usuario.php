f0779e525ab2915002bc6542ea51a35e3d52d4b0
AdrianTorresG <atg292001@hotmail.com>
2021-04-25 19:43:04 -0500

    usuario_validaciones

--- plantilla_currency exchange/php/usuario.php
+++ plantilla_currency exchange/php/usuario.php
@@ -2,7 +2,7 @@
 
 include_once "utilerias.php";
 $id_user_error = $id_comp_error = $nom_error = $contrasena_error = $rol_error = ""; 
-$id_user = $id_comp = $nom = $contrasena = $rol = $success = $option = $btnsn = $exist = "";
+$id_user = $id_comp = $nom = $contrasena = $rol = $success = $option = $btnsn = $exist = $val1 = $val2 = "";
 
 
 if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_altas"])){
@@ -37,35 +37,53 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_altas"])){
     $rol = test_input($_POST["rol"]);
     
     $estatus = 1;
-    
+
     if ($id_user_error == "" and $id_comp_error == "" and $nom_error == "" and $contrasena_error == "" and $rol_error == ""){
         $laContrasena = password_hash($contrasena, PASSWORD_DEFAULT);
-        $query="INSERT INTO Usuario(idCompania,idUsuario,nombre,contrasena,rol,estatus) VALUES ('$id_comp','$id_user','$nom','$laContrasena','$rol','$estatus')";
-        $sql=mysqli_query($conection,$query);
-        if (!$sql){
-            $query="SELECT * FROM Usuario WHERE idUsuario='$id_user' and estatus=false";
-            $exist = mysqli_query($conection, $query);
-            if (!$exist){
+
+        $query = "SELECT * FROM Compania WHERE idCompania='$id_comp' and estatus=true";
+        $val1 = mysqli_query($conection,$query);
+        if (!$val1){
+            $success = "Error en el alta del usuario.";
+            $id_comp_error = "El ID de compañía ingresado no existe en los registros.";
+        }
+        else{
+            $query = "SELECT * FROM Rol WHERE rol='$rol' and estatus=true";
+            $val2 = mysqli_query($conection,$query);
+            if (!$val2){
                 $success = "Error en el alta del usuario.";
-                $id_user = $id_comp = $nom = $contrasena = $rol = $estatus = "";
+                $rol_error = "El rol ingresado no existe en los registros.";
             }
             else{
-                $row = $exist-> fetch_assoc();
-                if ($row["estatus"] == "0"){
-                    $success = "El ID del usuario ya existe en la base de datos pero en modo inactivo.\n¿Quiere cambiar su modo a activo y actualizarlo con los datos que ya ingresó?";
-                    $btnsn = "Mostrar";
+                $query="INSERT INTO Usuario(idCompania,idUsuario,nombre,contrasena,rol,estatus) VALUES ('$id_comp','$id_user','$nom','$laContrasena','$rol','$estatus')";
+                $sql=mysqli_query($conection,$query);
+                if (!$sql){
+                    $query="SELECT * FROM Usuario WHERE idUsuario='$id_user' and estatus=false";
+                    $exist = mysqli_query($conection, $query);
+                    if (!$exist){
+                        $success = "Error en el alta del usuario.";
+                        $id_user = $id_comp = $nom = $contrasena = $rol = $estatus = "";
+                    }
+                    else{
+                        $row = $exist-> fetch_assoc();
+                        if ($row["estatus"] == "0"){
+                            $success = "El ID del usuario ya existe en la base de datos pero en modo inactivo.\n¿Quiere cambiar su modo a activo y actualizarlo con los datos que ya ingresó?";
+                            $btnsn = "Mostrar";
+                        }
+                        else{
+                            $success = "Error en el alta del usuario.";
+                            $id_user = $id_comp = $nom = $contrasena = $rol = $estatus = "";
+                        }
+                    }
                 }
                 else{
-                    $success = "Error en el alta del usuario.";
+                    $success = "Alta realizada con éxito.";
+                    crea_permiso($id_user, $rol);
                     $id_user = $id_comp = $nom = $contrasena = $rol = $estatus = "";
                 }
             }
         }
-        else{
-            $success = "Alta realizada con éxito.";
-            crea_permiso($id_user, $rol);
-            $id_user = $id_comp = $nom = $contrasena = $rol = $estatus = "";
-        }
+        
     }
 }
 
@@ -95,7 +113,7 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_bajas"])){
                 registro_baja($row, $_SESSION['usuario']);
             }
             else{
-                $success = "Error en la baja del usuario22.";
+                $success = "Error en la baja del usuario.";
             }
         }
         $id_comp = $id_user = $nom = $contrasena = $rol = "";
@@ -141,19 +159,35 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_actualizar"])){
             $success = "Error en la actualización de datos del usuario.";
         }
         else{
-            $row = $exist-> fetch_assoc();
-            if ($row["estatus"] == "1"){
-                $query="UPDATE Usuario SET idCompania='$id_comp', nombre='$nom', contrasena='$laContrasena', rol='$rol' WHERE idUsuario='$id_user' and estatus=true";
-                $sql=mysqli_query($conection,$query);
-                if(!$sql){
-                    $success = "Error en la actualización de datos del usuario.xx";
-                }else{
-                    crea_permiso($id_user, $rol);
-                    $success = "Actualización realizada con éxito.";
-                }
+            $query="SELECT * FROM Compania WHERE idCompania='$id_comp' and estatus=true";
+            $val1 = mysqli_query($conection, $query);
+            if (!$val1){
+                $success = "Error en la actualización del usuario.";
+                $id_comp_error = "El ID de compañía ingresado no existe en los registros.";
             }
             else{
-                $success = "Error en la actualización de datos del usuario.";
+                $query="SELECT * FROM Rol WHERE rol='$rol' and estatus=true";
+                $val2 = mysqli_query($conection, $query);
+                if (!$val2){
+                    $success = "Error en la actualización del usuario.";
+                    $id_comp_error = "El rol ingresado no existe en los registros.";
+                }
+                else{
+                    $row = $exist-> fetch_assoc();
+                    if ($row["estatus"] == "1"){
+                        $query="UPDATE Usuario SET idCompania='$id_comp', nombre='$nom', contrasena='$laContrasena', rol='$rol' WHERE idUsuario='$id_user' and estatus=true";
+                        $sql=mysqli_query($conection,$query);
+                        if(!$sql){
+                            $success = "Error en la actualización de datos del usuario.";
+                        }else{
+                            crea_permiso($id_user, $rol);
+                            $success = "Actualización realizada con éxito.";
+                        }
+                    }
+                    else{
+                        $success = "Error en la actualización de datos del usuario.";
+                    }
+                }
             }
         }
        $id_user = $id_comp = $nom = $contrasena = $rol = "";
@@ -211,29 +245,45 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["confirmoc"])){
     //$rol = test_input($_POST["rol"]);
     
     if ($id_user_error == "" and $id_comp_error == "" and $nom_error == "" and $contrasena_error == "" and $rol_error == ""){
+        $laContrasena = password_hash($contrasena, PASSWORD_DEFAULT);
         $query="SELECT * FROM Usuario WHERE idUsuario='$id_user' and estatus=false";
         $exist = mysqli_query($conection, $query);
         if (!$exist){
             $success = "Error en la actualización de datos del usuario.";
         }
         else{
-            $row = $exist-> fetch_assoc();
-            if ($row["estatus"] == "0"){
-                $laContrasena = password_hash($contrasena, PASSWORD_DEFAULT);
-                $query="UPDATE Usuario SET nombre='$nom', idCompania='$id_comp', rol='$rol', estatus=true, contrasena='$laContrasena' WHERE idUsuario='$id_user'";
-                $sql=mysqli_query($conection,$query);
-                if(!$sql){
-                    $success = "Error en la actualización de datos del usuario.";
-                }else{
-                    crea_permiso($id_user, $rol);
-                    $success = "Alta y actualización realizada con éxito.";
-                }
+            $query="SELECT * FROM Compania WHERE idCompania='$id_comp' and estatus=true";
+            $val1 = mysqli_query($conection, $query);
+            if (!$val1){
+                $success = "Error en la actualización del usuario.";
+                $id_comp_error = "El ID de compañía ingresado no existe en los registros.";
             }
             else{
-                $success = "Error en la actualización de datos del almacen.";
+                $query="SELECT * FROM Rol WHERE rol='$rol' and estatus=true";
+                $val2 = mysqli_query($conection, $query);
+                if (!$val2){
+                    $success = "Error en la actualización del usuario.";
+                    $id_comp_error = "El rol ingresado no existe en los registros.";
+                }
+                else{
+                    $row = $exist-> fetch_assoc();
+                    if ($row["estatus"] == "0"){
+                        $query="UPDATE Usuario SET idCompania='$id_comp', nombre='$nom', contrasena='$laContrasena', rol='$rol', estatus=true WHERE idUsuario='$id_user'";
+                        $sql=mysqli_query($conection,$query);
+                        if(!$sql){
+                            $success = "Error en la actualización de datos del usuario.";
+                        }else{
+                            crea_permiso($id_user, $rol);
+                            $success = "Actualización realizada con éxito.";
+                        }
+                    }
+                    else{
+                        $success = "Error en la actualización de datos del usuario.";
+                    }
+                }
             }
         }
-        $id_user = $id_comp = $nom = $contrasena = $rol = "";
+       $id_user = $id_comp = $nom = $contrasena = $rol = "";
     }
 }
 
