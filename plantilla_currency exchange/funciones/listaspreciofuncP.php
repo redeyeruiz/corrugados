2bb7c8e0908735915eabebfafe14b77d523d7249
AdrianTorresG <atg292001@hotmail.com>
2021-04-26 03:28:49 -0500

    inv-val

--- plantilla_currency exchange/funciones/listaspreciofuncP.php
+++ plantilla_currency exchange/funciones/listaspreciofuncP.php
@@ -2,7 +2,7 @@
 
 include_once "util_pcP.php";
 $idcomp_error = $idlis_error = $idart_error = $desc_error = $prec_error = $olmp_error = $lvldesc_error = $fcad_error = $finicio_error = $impdesc_error = ""; 
-$idcomp = $idlis = $idart = $success = $option = $desc = $prec = $olmp = $lvldesc = $fcad = $finicio = $impdesc = $exist = $btnsn = "";
+$idcomp = $idlis = $idart = $success = $option = $desc = $prec = $olmp = $lvldesc = $fcad = $finicio = $impdesc = $exist = $btnsn = $val1 = $val2 = "";
 
 if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_altas"])){
     if (empty($_POST["idcomp"])){
@@ -68,30 +68,48 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_altas"])){
 
     if ($idcomp_error == "" and $idlis_error == "" and $idart_error == "" and $desc_error == "" and $prec_error == "" and $olmp_error == "" and $lvldesc_error == "" and $fcad_error == "" and $finicio_error == "" and $impdesc_error == ""){
         //$conection = mysqli_connect("localhost", "root", "rootroot", "PapelesCorrugados");
-        $query="INSERT INTO ListaPrecio (idCompania, idLista, idArticulo, descuento, precio, cantOlmp, nivelDscto, fechaCaducidad, fechaInicio, impDesc, estatus) VALUES ('$idcomp','$idlis','$idart','$desc','$prec','$olmp','$lvldesc','$fcad','$finicio','$impdesc', true);";
-        $sql=mysqli_query($conection,$query);
-        if (!$sql){
-            $query="SELECT * FROM ListaPrecio WHERE idLista='$idlis' and idCompania='$idcomp' and idArticulo='$idart' and estatus=false";
-            $exist = mysqli_query($conection, $query);
-            if (!$exist){
-                $success = "Error en el alta de la lista.";
-                $idcomp = $idlis = $idart = $desc = $prec = $olmp = $lvldesc = $fcad = $finicio = $impdesc = "";
+        $query = "SELECT * FROM Compania WHERE idCompania='$idcomp'";
+        $val1 = mysqli_query($conection,$query);
+        $row = $val1-> fetch_assoc();
+        if ($row["estatus"] == "0"){
+            $success = "Error en el alta del inventario.";
+            $id_comp_error = "El ID de compañía ingresado no existe en los registros.";
+        }
+        else{
+            $query = "SELECT * FROM Articulo WHERE idArticulo='$idart'";
+            $val2 = mysqli_query($conection,$query);
+            $row = $val2-> fetch_assoc();
+            if ($row["estatus"] == "0"){
+                $success = "Error en el alta de la lista de precios.";
+                $id_comp_error = "El ID del artículo ingresado no existe en los registros.";
             }
             else{
-                $row = $exist-> fetch_assoc();
-                if ($row["estatus"] == "0"){
-                    $success = "La lista con los IDs ingresados ya existe en la base de datos pero en modo inactivo.\n¿Quiere cambiar su modo a activo y actualizarlo con los datos que ya ingresó?";
-                    $btnsn = "Mostrar";
+                $query="INSERT INTO ListaPrecio (idCompania, idLista, idArticulo, descuento, precio, cantOlmp, nivelDscto, fechaCaducidad, fechaInicio, impDesc, estatus) VALUES ('$idcomp','$idlis','$idart','$desc','$prec','$olmp','$lvldesc','$fcad','$finicio','$impdesc', true);";
+                $sql=mysqli_query($conection,$query);
+                if (!$sql){
+                    $query="SELECT * FROM ListaPrecio WHERE idLista='$idlis' and idCompania='$idcomp' and idArticulo='$idart' and estatus=false";
+                    $exist = mysqli_query($conection, $query);
+                    if (!$exist){
+                        $success = "Error en el alta de la lista.";
+                        $idcomp = $idlis = $idart = $desc = $prec = $olmp = $lvldesc = $fcad = $finicio = $impdesc = "";
+                    }
+                    else{
+                        $row = $exist-> fetch_assoc();
+                        if ($row["estatus"] == "0"){
+                            $success = "La lista con los IDs ingresados ya existe en la base de datos pero en modo inactivo.\n¿Quiere cambiar su modo a activo y actualizarlo con los datos que ya ingresó?";
+                            $btnsn = "Mostrar";
+                        }
+                        else{
+                            $success = "Error en el alta de la lista.";
+                            $idcomp = $idlis = $idart = $desc = $prec = $olmp = $lvldesc = $fcad = $finicio = $impdesc = "";
+                        }
+                    }
                 }
                 else{
-                    $success = "Error en el alta de la lista.";
+                    $success = "Alta realizada con éxito.";
                     $idcomp = $idlis = $idart = $desc = $prec = $olmp = $lvldesc = $fcad = $finicio = $impdesc = "";
                 }
             }
-        }
-        else{
-            $success = "Alta realizada con éxito.";
-            $idcomp = $idlis = $idart = $desc = $prec = $olmp = $lvldesc = $fcad = $finicio = $impdesc = "";
         }
     }
 }
@@ -205,14 +223,32 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["b_actualizar"])){
             $success = "Error en la actualización de datos de la lista.";
         }
         else{
-            $row = $exist-> fetch_assoc();
-            if ($row["estatus"] == "1"){
-                $query="UPDATE ListaPrecio SET descuento='$desc', precio='$prec', cantOlmp='$olmp', nivelDscto='$lvldesc', fechaCaducidad='$fcad', fechaInicio='$finicio', impDesc='$impdesc' WHERE idLista='$idlis' AND idCompania='$idcomp' AND idArticulo='$idart' AND estatus=true";
-                $sql=mysqli_query($conection,$query);
-                $success = "Actualización realizada con éxito.";
+            $query = "SELECT * FROM Compania WHERE idCompania='$idcomp'";
+            $val1 = mysqli_query($conection,$query);
+            $row = $val1-> fetch_assoc();
+            if ($row["estatus"] == "0"){
+                $success = "Error en el alta del inventario.";
+                $id_comp_error = "El ID de compañía ingresado no existe en los registros.";
             }
             else{
-                $success = "Error en la actualización de datos de la lista.";
+                $query = "SELECT * FROM Articulo WHERE idArticulo='$idart'";
+                $val2 = mysqli_query($conection,$query);
+                $row = $val2-> fetch_assoc();
+                if ($row["estatus"] == "0"){
+                    $success = "Error en el alta de la lista de precios.";
+                    $id_comp_error = "El ID del artículo ingresado no existe en los registros.";
+                }
+                else{
+                    $row = $exist-> fetch_assoc();
+                    if ($row["estatus"] == "1"){
+                        $query="UPDATE ListaPrecio SET descuento='$desc', precio='$prec', cantOlmp='$olmp', nivelDscto='$lvldesc', fechaCaducidad='$fcad', fechaInicio='$finicio', impDesc='$impdesc' WHERE idLista='$idlis' AND idCompania='$idcomp' AND idArticulo='$idart' AND estatus=true";
+                        $sql=mysqli_query($conection,$query);
+                        $success = "Actualización realizada con éxito.";
+                    }
+                    else{
+                        $success = "Error en la actualización de datos de la lista.";
+                    }
+                }
             }
         }
         $idcomp = $idlis = $idart = $desc = $prec = $olmp = $lvldesc = $fcad = $finicio = $impdesc = "";
@@ -312,14 +348,32 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["confirmoc"])){
             $success = "Error en la actualización de datos de la lista.";
         }
         else{
-            $row = $exist-> fetch_assoc();
+            $query = "SELECT * FROM Compania WHERE idCompania='$idcomp'";
+            $val1 = mysqli_query($conection,$query);
+            $row = $val1-> fetch_assoc();
             if ($row["estatus"] == "0"){
-                $query="UPDATE ListaPrecio SET descuento='$desc', precio='$prec', cantOlmp='$olmp', nivelDscto='$lvldesc', fechaCaducidad='$fcad', fechaInicio='$finicio', impDesc='$impdesc', estatus=true WHERE idLista='$idlis' and idCompania='$idcomp' and idArticulo='$idart'";
-                $sql=mysqli_query($conection,$query);
-                $success = "Alta y actualización realizada con éxito.";
+                $success = "Error en el alta del inventario.";
+                $id_comp_error = "El ID de compañía ingresado no existe en los registros.";
             }
             else{
-                $success = "Error en la actualización de datos de la lista.";
+                $query = "SELECT * FROM Articulo WHERE idArticulo='$idart'";
+                $val2 = mysqli_query($conection,$query);
+                $row = $val2-> fetch_assoc();
+                if ($row["estatus"] == "0"){
+                    $success = "Error en el alta de la lista de precios.";
+                    $id_comp_error = "El ID del artículo ingresado no existe en los registros.";
+                }
+                else{
+                    $row = $exist-> fetch_assoc();
+                    if ($row["estatus"] == "0"){
+                        $query="UPDATE ListaPrecio SET descuento='$desc', precio='$prec', cantOlmp='$olmp', nivelDscto='$lvldesc', fechaCaducidad='$fcad', fechaInicio='$finicio', impDesc='$impdesc', estatus=true WHERE idLista='$idlis' and idCompania='$idcomp' and idArticulo='$idart'";
+                        $sql=mysqli_query($conection,$query);
+                        $success = "Alta y actualización realizada con éxito.";
+                    }
+                    else{
+                        $success = "Error en la actualización de datos de la lista.";
+                    }
+                }
             }
         }
         $idcomp = $idlis = $idart = $desc = $prec = $olmp = $lvldesc = $fcad = $finicio = $impdesc = "";
